0323f57d9da92a8e95958a167ed8d152beaf7af0
Zerihun <Zerihunemail@gmail.com>
2021-12-04 12:11:14 +0300

    .

--- client/client.go
+++ client/client.go
@@ -2,28 +2,62 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
 	pb "gRPCTest/helloworld"
+	"io/ioutil"
 	"log"
 	"time"
 
-	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+
+	"google.golang.org/grpc"
+)
+
+const (
+	Addr       = ":50052"
+	AkbarCA    = "cert/adinterface.adsrecognition.com.crt"
+	ClientCert = "cert/client.crt"
+	ClientKey  = "cert/client.key"
+	ServerName = "server"
 )
 
 func main() {
 	log.Println("Client running ...")
 
-	creds, err := credentials.NewClientTLSFromFile("../ca.cert", "operatorsvps2.adsrecognition.com")
+	// Load certs from the d
+	cert, err := tls.LoadX509KeyPair(ClientCert, ClientKey)
 	if err != nil {
-		log.Fatalln(err)
+		fmt.Errorf("Could not load client key pair : %v", err)
 	}
 
+	// Create certpool from the CA
+	certPool := x509.NewCertPool()
+	ca, err := ioutil.ReadFile(AkbarCA)
+	if err != nil {
+		fmt.Errorf("Could not read Cert CA : %v", err)
+	}
+
+	// Append the certs from the CA
+	if ok := certPool.AppendCertsFromPEM(ca); !ok {
+		fmt.Errorf("Failed to append CA cert : %v", err)
+	}
+
+	// Create transport creds based on TLS.
+	creds := credentials.NewTLS(&tls.Config{
+		ServerName:   ServerName,
+		Certificates: []tls.Certificate{cert},
+		RootCAs:      certPool,
+	})
+
 	opts := []grpc.DialOption{
+		// grpc.WithInsecure(),
 		grpc.WithTransportCredentials(creds),
 		grpc.WithBlock(),
 	}
 
-	conn, err := grpc.Dial(":8888", opts...)
+	conn, err := grpc.Dial("operatorsvps2.adsrecognition.com:50051", opts...)
 	if err != nil {
 		log.Fatalln(err)
 	}
--- main.go
+++ main.go
@@ -3,12 +3,23 @@ package main
 import (
 	"context"
 	"crypto/tls"
+	"crypto/x509"
+	"fmt"
 	pb "gRPCTest/helloworld"
+	"io/ioutil"
 	"log"
 	"net"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+)
+
+// Cert file details
+const (
+	AkbarCA    = "cert/adinterface.adsrecognition.com.crt"
+	ServerCert = "cert/server.crt"
+	ServerKey  = "cert/server.key"
+	ServerName = "server"
 )
 
 // server is used to implement helloworld.GreeterServer.
@@ -19,14 +30,34 @@ type server struct {
 func main() {
 	log.Println("Server running ...")
 
-	cert, err := tls.LoadX509KeyPair("service.pem", "service.key")
+	// Load certs from the disk.
+	cert, err := tls.LoadX509KeyPair(ServerCert, ServerKey)
 	if err != nil {
-		log.Fatalln(err)
+		fmt.Errorf("could not server key pairs: %s", err)
 	}
 
+	// Create certpool from the CA
+	certPool := x509.NewCertPool()
+	ca, err := ioutil.ReadFile(AkbarCA)
+	if err != nil {
+		fmt.Printf("could not read CA cert: %s", err)
+	}
+
+	// Append the certs from the CA
+	if ok := certPool.AppendCertsFromPEM(ca); !ok {
+		fmt.Printf("Failed to append the CA certs: %s", err)
+	}
+
+	// Create the TLS config for gRPC server.
+	creds := credentials.NewTLS(
+		&tls.Config{
+			ClientAuth:   tls.RequireAnyClientCert,
+			Certificates: []tls.Certificate{cert},
+			ClientCAs:    certPool,
+		})
 	opts := []grpc.ServerOption{
 		// Enable TLS for all incoming connections.
-		grpc.Creds(credentials.NewServerTLSFromCert(&cert)),
+		grpc.Creds(creds),
 	}
 
 	lis, err := net.Listen("tcp", ":8888")
